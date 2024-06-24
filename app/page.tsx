a7e29c3f3877824468646e82dfc15e97ee582c4b
sarthak <sarthakgaud04@gmail.com>
2024-06-24 18:23:24 +0530

    paths

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,9 @@ export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <h1> Intern assignment</h1>
-      <h2> By- Sarthak Gaud</h2>
-      <h4> Mail - sarthakgaud06@gmail.com</h4>
-
+      <h1> By- Sarthak Gaud</h1>
+      <h1> Mail - sarthakgaud06@gmail.com</h1>
+        <h1>PATHS: /money, /color</h1>
     </main>
   );
 }
